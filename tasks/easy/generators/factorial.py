fe20fd91958c6b4f5e4a717e43a70ff8be6b0ca9
Olga Santarovich <santarovich@gmail.com>
2022-02-13 16:50:57 +0300

    easy\generators\factorial done

--- tasks/easy/generators/factorial.py
+++ tasks/easy/generators/factorial.py
@@ -10,3 +10,17 @@ next(factorial_gen) -> 2
 next(factorial_gen) -> 6
 next(factorial_gen) -> 24
 """
+
+
+def factorial():
+    number = 1
+    for i in range(1, 5):
+        number *= i
+        yield number
+
+
+factorial_gen = factorial()
+print(next(factorial_gen))
+print(next(factorial_gen))
+print(next(factorial_gen))
+print(next(factorial_gen))
