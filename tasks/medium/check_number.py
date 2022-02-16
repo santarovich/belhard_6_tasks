01a55b6c425f3bbfa10410fb21df5ad4e88b76ca
Olga Santarovich <santarovich@gmail.com>
2022-02-16 19:43:56 +0300

    medium\check_number done

--- tasks/medium/check_number.py
+++ tasks/medium/check_number.py
@@ -5,3 +5,27 @@ False, если нет
 
 Нельзя пользоваться операцией возведения в степень
 """
+
+
+def check_number(n: int) -> int:
+    if n == 0:
+        return False
+    if n == 1:
+        return True
+    if n % 2 != 0:
+        return False
+    check_number(n // 2)
+    return True
+
+
+# def check_number(n: int) -> int:
+#     if n == 0:
+#         return False
+#     while n != 1:
+#         if n % 2 != 0:
+#             return False
+#         n = n // 2
+#     return True
+
+
+# print(check_number(16))
