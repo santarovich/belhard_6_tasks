67a37a579a14740ff40e02b9d3458c2027594bd1
Olga Santarovich <santarovich@gmail.com>
2022-02-16 14:11:43 +0300

    hard\generate_brackets done

--- tasks/hard/generate_brackets.py
+++ tasks/hard/generate_brackets.py
@@ -31,3 +31,20 @@ n = 4
 ()()(())
 ()()()()
 """
+
+
+def generate_brackets(
+    n: int,
+    sequence: str = "",
+    opened: int = 0,
+    closed: int = 0
+) -> None:
+    if opened < n:
+        generate_brackets(n, sequence + "(", opened + 1, closed)
+    if closed < opened:
+        generate_brackets(n, sequence + ")", opened, closed + 1)
+    if len(sequence) == n * 2:
+        print(sequence)
+
+
+# generate_brackets(3)
